22d95fa194f7f17b446119e52e9064fbe5d84610
lsmith <therealshaunsred@gmail.com>
2023-08-15 13:56:18 -0400

    Added User Show Page

--- controllers/users.js
+++ controllers/users.js
@@ -3,5 +3,19 @@ const Comment = require("../models/comment");
 const User = require("../models/user");
 
 module.exports = {
-    
+    show,
+}
+
+async function show(req, res, next){
+    try {
+        const user = await User.findById(req.params.id)
+        res.render("users/show", {
+          title: "User Details",
+          user,
+        })
+      
+      } catch (err) {
+         console.log(err)
+         next(Error(err)) 
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const ensureLoggedIn = require('../config/ensureLoggedIn');
 const usersCtl = require('../controllers/users')
 
-
+router.get('/:id', usersCtl.show);
 
 module.exports = router;
